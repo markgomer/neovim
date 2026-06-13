438632d74f123d1c575b790186240ce17e1ca2d4
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-06-13 15:45:23 -0300

    fix some transparency bugs in kanagawa

--- lua/vimarco/theme.lua
+++ lua/vimarco/theme.lua
@@ -1,11 +1,32 @@
 vim.pack.add { gh "rebelot/kanagawa.nvim" }
 
-require('kanagawa').setup {
+require('kanagawa').setup({
     transparent = true,
     commentStyle = { italic = true },
     functionStyle = { italic = true },
     keywordStyle = { italic = true},
     statementStyle = { bold = true },
-}
+    colors = {
+        theme = {
+            all = {
+                ui = {
+                    bg_gutter = "none"
+                }
+            }
+        }
+    },
+})
 
-require("kanagawa").load()
+require("kanagawa").load("wave")
+
+-- Fire AFTER all plugins init
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopePromptBorder",  { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
+        vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
+    end,
+})
+
