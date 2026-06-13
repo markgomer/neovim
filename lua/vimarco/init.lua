9c7a4e810afbb86a071da75fdc1cc46ca145970c
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-06-13 16:16:58 -0300

    add treesitter and vimpack functions

--- lua/vimarco/init.lua
+++ lua/vimarco/init.lua
@@ -2,10 +2,12 @@ function gh(repo) return 'https://github.com/' .. repo end
 
 require("vimarco.options")
 require("vimarco.keymap")
+require("vimarco.vimpack")
 require("vimarco.telescope")
 require("vimarco.which-key")
 require("vimarco.neotree")
 require("vimarco.theme")
+require("vimarco.treesitter")
 
 vim.g.have_nerd_font = true
 if vim.g.have_nerd_font then vim.pack.add { gh 'nvim-tree/nvim-web-devicons' } end
