ecdb9a0bb68e4b3cdca93a9586bf38326e093853
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-06-13 12:17:41 -0300

    ajustments to telescope

--- lua/vimarco/telescope.lua
+++ lua/vimarco/telescope.lua
@@ -1,47 +1,32 @@
----@type (string|vim.pack.Spec)[]
-local telescope_plugins = {
-	gh 'nvim-lua/plenary.nvim',
-	gh 'nvim-telescope/telescope.nvim',
-	gh 'nvim-telescope/telescope-ui-select.nvim',
+vim.pack.add({
+    gh 'nvim-lua/plenary.nvim',
+    gh 'nvim-telescope/telescope.nvim',
+    gh 'nvim-telescope/telescope-ui-select.nvim',
+    gh 'nvim-telescope/telescope-fzf-native.nvim'
+})
+
+-- See `:help telescope` and `:help telescope.setup()`
+require('telescope').setup {
+    extensions = {
+        ['ui-select'] = { require('telescope.themes').get_dropdown() },
+    },
 }
 
-if vim.fn.executable 'make' == 1 then table.insert(telescope_plugins, gh 'nvim-telescope/telescope-fzf-native.nvim') end
+-- Enable Telescope extensions if they are installed
+pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'ui-select')
 
-  -- NOTE: You can install multiple plugins at once
-  vim.pack.add(telescope_plugins)
-
-  -- See `:help telescope` and `:help telescope.setup()`
-  require('telescope').setup {
-	  -- You can put your default mappings / updates / etc. in here
-	  --  All the info you're looking for is in `:help telescope.setup()`
-	  --
-	  -- defaults = {
-		  --   mappings = {
-			  --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-			  --   },
-			  -- },
-			  -- pickers = {}
-	  --
-	  extensions = {
-		  ['ui-select'] = { require('telescope.themes').get_dropdown() },
-	  },
-  }
-
-  -- Enable Telescope extensions if they are installed
-  pcall(require('telescope').load_extension, 'fzf')
-  pcall(require('telescope').load_extension, 'ui-select')
-
-  -- See `:help telescope.builtin`
-  local builtin = require 'telescope.builtin'
-  vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-  vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-  vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-  vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-  vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-  vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-  vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-  vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-  vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
-  vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
+-- See `:help telescope.builtin`
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find Help' })
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Find Keymaps' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
+vim.keymap.set('n', '<leader>ft', builtin.builtin, { desc = 'Find Select Telescope' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Find by Grep' })
+vim.keymap.set('n', '<leader>fp', builtin.git_files, { desc = 'Find Project Git Files' })
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Find Diagnostics' })
+vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = 'Find Resume' })
+vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = 'Find Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = 'Find Commands' })
+vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Find existing buffers' })
+vim.keymap.set({ 'n', 'v' }, '<leader>fw', builtin.grep_string, { desc = 'Find current Word' })
