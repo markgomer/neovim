13287d9be27aa931decd1f6822b729d074c8d9e6
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-06-15 10:36:48 -0300

    add some more snacks toggles

--- lua/vimarco/snacks.lua
+++ lua/vimarco/snacks.lua
@@ -123,10 +123,21 @@ local function setup_snacks_keys()
             wo = { spell = false, wrap = false, signcolumn = "yes", statuscolumn = " ", conceallevel = 3 },
         })
     end, { desc = "Neovim News" })
+
+    Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+    Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+    Snacks.toggle.diagnostics():map("<leader>ud")
+    Snacks.toggle.line_number():map("<leader>ul")
+    Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
+    Snacks.toggle.treesitter():map("<leader>uT")
+    Snacks.toggle.inlay_hints():map("<leader>uh")
+    Snacks.toggle.indent():map("<leader>ug")
+    Snacks.toggle.dim():map("<leader>uD")
 end
 
 
 init = function()
+    local map = vim.keymap.set
     vim.api.nvim_create_autocmd("User", {
         pattern = "VeryLazy",
         callback = function()
