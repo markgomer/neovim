1ac32baf8a1b2d0aeaa3ec9f9a25263eea82679b
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-06-15 10:15:34 -0300

    tweak blink settings

--- lua/vimarco/blink.lua
+++ lua/vimarco/blink.lua
@@ -35,22 +35,21 @@ require('blink.cmp').setup {
         -- <c-k>: Toggle signature help
         --
         -- See `:help blink-cmp-config-keymap` for defining your own keymap
-        preset = 'default',
+        preset = 'enter',
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
     },
-
     appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
+        nerd_font_variant = 'normal', -- or 'mono'
     },
 
     completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 0 },
     },
 
     sources = {
