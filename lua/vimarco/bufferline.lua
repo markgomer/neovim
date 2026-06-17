e5f4c424fc1ee8df1aa213748b24225490e62009
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-06-17 15:28:54 -0300

    add diagnostics options

--- lua/vimarco/bufferline.lua
+++ lua/vimarco/bufferline.lua
@@ -10,6 +10,27 @@ local bufferline_opts = {
         pick = {
             alphabet = "1234567890ABCDEF",
         },
+        diagnostics = "nvim_lsp", 
+
+        -- This function now works because diagnostics are enabled
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = " "
+            for e, n in pairs(diagnostics_dict) do
+                local sym = e == "error" and " "
+                or (e == "warning" and " " or " ")
+                s = s .. n .. sym .. " "
+            end
+            return s
+        end,
+
+        offsets = {
+            {
+                filetype = "Snacks",
+                text = "File Explorer",
+                highlight = "Directory",
+                separator = true -- use a "true" to enable the default, or set your own character
+            }
+        }
     }
 }
 
--- lua/vimarco/options.lua
+++ lua/vimarco/options.lua
@@ -21,3 +21,23 @@ vim.opt.laststatus = 3 -- global statusline
 vim.opt.showmode = false -- Dont show mode since we have a statusline
 
 vim.opt.termguicolors = true
+
+-- DIAGNOSTICS
+-- Change text/icon inside sign column
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.diagnostic.config({
+    virtual_text = true, -- This puts diagnostic inline with code
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = " ", -- Change to icon you like
+            [vim.diagnostic.severity.WARN]  = " ",
+            [vim.diagnostic.severity.HINT]  = "󰌵 ",
+            [vim.diagnostic.severity.INFO]  = " ",
+        },
+    }
+})
