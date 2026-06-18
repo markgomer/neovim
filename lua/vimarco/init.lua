516c4b145c6e38ca3b28902ebecab58201585b18
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-06-18 19:34:46 -0300

    add mini statusline

--- lua/vimarco/init.lua
+++ lua/vimarco/init.lua
@@ -1,10 +1,18 @@
 function gh(repo) return 'https://github.com/' .. repo end
 
+
+vim.pack.add { gh 'nvim-tree/nvim-web-devicons' }
+
+vim.pack.add({
+    { src = gh "nvim-mini/mini.nvim", version = "stable" },
+})
+
 require("vimarco.options")
 require("vimarco.keymap")
 require("vimarco.theme")
 
 require("vimarco.snacks")
+require("vimarco.mini")
 
 require("vimarco.which-key")
 require("vimarco.treesitter")
@@ -16,10 +24,3 @@ require("vimarco.gitsigns")
 require("vimarco.obsidian")
 require("vimarco.transparent")
 require("vimarco.vimpack")
-
-vim.g.have_nerd_font = true
-if vim.g.have_nerd_font then vim.pack.add { gh 'nvim-tree/nvim-web-devicons' } end
-
--- Highlight todo, notes, etc in comments
-vim.pack.add { gh 'folke/todo-comments.nvim' }
-require('todo-comments').setup { signs = false }
---
+++ lua/vimarco/mini.lua
@@ -0,0 +1,5 @@
+-- Simple and easy statusline.
+--  You could remove this setup call if you don't like it,
+--  and try some other statusline plugin
+local statusline = require("mini.statusline")
+statusline.setup({ use_icons = true })
