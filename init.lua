64ec69ffd0b172e1b54dc1faa409aa26bc080b42
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-06-18 22:38:57 -0300

    move vimarco.init contents to init root

--- init.lua
+++ init.lua
@@ -1,3 +1,29 @@
 vim.loader.enable()
 
-require("vimarco")
+function gh(repo) return 'https://github.com/' .. repo end
+
+
+vim.pack.add { gh 'nvim-tree/nvim-web-devicons' }
+
+vim.pack.add({
+    { src = gh "nvim-mini/mini.nvim", version = "stable" },
+})
+
+require("vimarco.options")
+require("vimarco.keymap")
+require("vimarco.theme")
+
+require("vimarco.snacks")
+require("vimarco.mini")
+
+require("vimarco.which-key")
+require("vimarco.treesitter")
+require("vimarco.lsp")
+require("vimarco.blink")
+require("vimarco.trouble")
+require("vimarco.formatter")
+require("vimarco.gitsigns")
+require("vimarco.obsidian")
+require("vimarco.transparent")
+require("vimarco.persistence")
+require("vimarco.vimpack")
