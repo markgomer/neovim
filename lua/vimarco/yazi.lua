2019a96cbceafe4d9b7a82bb4ec7e46bf7ad316b
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-07-07 10:47:31 -0300

    remove inactive code from yazi plugin configuration

--- lua/vimarco/yazi.lua
+++ lua/vimarco/yazi.lua
@@ -3,20 +3,15 @@ vim.pack.add({
 	gh("mikavilpas/yazi.nvim"),
 })
 
-require("yazi").setup({
-    open_for_directories = false,
+vim.g.loaded_netrwPlugin = 1
+vim.api.nvim_create_autocmd("UIEnter", {
+    callback = function()
+        require("yazi").setup({
+            open_for_directories = true,
+        })
+    end,
 })
-
 
 vim.keymap.set("n", "<leader>e", function()
 	require("yazi").yazi()
 end)
-
-vim.g.loaded_netrwPlugin = 1
-vim.api.nvim_create_autocmd("UIEnter", {
-  callback = function()
-    require("yazi").setup({
-      open_for_directories = true,
-    })
-  end,
-})
